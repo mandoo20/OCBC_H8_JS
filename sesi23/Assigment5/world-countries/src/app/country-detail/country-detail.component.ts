f10795a849e91031d70477042887a7572bad92d9
mandoo20 <armandofransisco20@gmail.com>
2021-11-01 22:11:00 +0700

    commit Assgiment 5 rev go back button

--- sesi23/Assigment5/world-countries/src/app/country-detail/country-detail.component.ts
+++ sesi23/Assigment5/world-countries/src/app/country-detail/country-detail.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Country } from '../country';
@@ -6,30 +7,40 @@ import { CountryService } from '../country.service';
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
-  styleUrls: ['./country-detail.component.css']
+  styleUrls: ['./country-detail.component.css'],
 })
 export class CountryDetailComponent implements OnInit {
-
   country_name: string;
   countrydet!: Country;
+  
 
-  constructor(private actRoute: ActivatedRoute,private countryService: CountryService) {
-      this.country_name = this.actRoute.snapshot.params.name;
-   }
+  constructor(
+    private actRoute: ActivatedRoute,
+    private countryService: CountryService,
+    private _location: Location
+  ) {
+    this.country_name = this.actRoute.snapshot.params.name;
+  }
 
   ngOnInit(): void {
     this.getOneCountry(this.country_name);
-    this.countrydet.population = this.numberWithCommas(this.countrydet.population);
+    this.countrydet.population = this.numberWithCommas(
+      this.countrydet.population
+    );
     this.countrydet.area = this.numberWithCommas(this.countrydet.area);
     this.countrydet.GDP = this.numberWithCommas(this.countrydet.GDP);
   }
 
-  getOneCountry(name:string) {
-    this.countryService.getDetailCountry(name).subscribe(countries => this.countrydet = countries);
+  getOneCountry(name: string) {
+    this.countryService
+      .getDetailCountry(name)
+      .subscribe((countries) => (this.countrydet = countries));
   }
 
-   numberWithCommas(number:string) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
+  numberWithCommas(number: string) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  backClicked() {
+    this._location.back();
+  }
 }
